0676facc7964087d3210b4893683ce1756e8d8d5
pavankumar konakondla <pavankumar.konakondla@gmail.com>
2017-11-11 16:01:33 -0600

    bundleing and Codemap added

--- buildScripts/srcServer.js
+++ buildScripts/srcServer.js
@@ -1,9 +1,19 @@
 import express from 'express';
 import path from 'path';
 import open from 'open';
+import webpack from 'webpack';
+import config from '../webpack.config.dev';
+
 
 const port = 3000;
 const app = express();
+const complier= webpack(config);
+
+app.use(require('webpack-dev-middleware')(complier,{
+  noinfo:true,
+  publicpath:config.output.publicpath
+}))
+
 //routeing
 app.get('/',function(req,res){
 res.sendFile(path.join(__dirname,'../src/index.html'));
